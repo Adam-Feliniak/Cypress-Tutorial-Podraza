f1033853da1894b81ba3f8d24e576c2ef2a17b94
Adam Feliniak <adddzik@gmail.com>
2024-01-27 23:36:10 +0100

    Excercise: loops

---
+++ petle.js
@@ -0,0 +1,19 @@
+// //FOR
+// for(let i  = 20; i>10; i--) {
+//     console.log("Testowanie pętli for- " +i)
+// }
+
+// //WHILE
+// let licznik = 0;
+// while (licznik <10){
+//     console.log("Testowanie pętli WHILE")
+//     licznik++;
+// }
+
+//DO WHILE
+let licznik = 0;
+
+do {
+    console.log("Testowanie pętli DO WHILE")
+    licznik++;
+} while(licznik > 10)
